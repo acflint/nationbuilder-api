15c50956f7b66dcf7dc19f3959d2b476cec73a0c
Alex F <acflint@users.noreply.github.com>
2023-09-26 16:01:33 -0700

    updating UrlBuilder

--- lib/nationbuilder/utils.rb
+++ lib/nationbuilder/utils.rb
@@ -3,8 +3,7 @@ module NationBuilder::Utils
     module_function
 
     def call(nation, path)
-      url_string = path.include?("http") ? path : nation.url + path
-
+      url_string = path.include?("http") ? path : "https://#{nation[:slug]}.nationbuilder.com" + path
       uri = URI.parse(url_string)
       new_query_ar = URI.decode_www_form(String(uri.query)) << ["token", nation[:token]]
       uri.query = URI.encode_www_form(new_query_ar)
